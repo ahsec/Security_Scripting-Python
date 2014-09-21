a56fbd21ca6b529cb488dbc3f0c1b3b0af08ddce
Angel Hernandez <ah.sec7@gmail.com>
2014-09-21 12:39:55 -0500

    Modified version of file_stats.py The output for last access time and last modification time is human readable now :)

--- file_stats.py
+++ file_stats.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python
 import os
 import sys
-
+import time
 
 # Exercise number 2 of the SecurityTube Python Scripting Expert.
 # For any given filename list out all the stats related to the file 
 # such as size, creation time, etc
 
 def print_stats(filename):
-  last_access_t = os.path.getatime(filename)
-  last_modif_t = os.path.getmtime(filename)
+  last_access_t = convertTimes(os.path.getatime(filename))
+  last_modif_t = convertTimes(os.path.getmtime(filename))
   size = os.path = os.path.getsize(filename)
   print 'Filename: ' + filename 
   print 'Time of Last Access: ' + str(last_access_t)
   print 'Time of Last Modification: ' + str(last_modif_t)
   # Convert size from blocks to Kilobytes
   print 'Size of the file: ' + str(size * .0043716) + 'K'
+
+
+def convertTimes(modTime):
+  # Function to convert strange timing into a more human readable form 
+  # Uses local time
+  modTime_hr = time.localtime(modTime)
+  realTime = time.strftime("%m/%d/%Y %H:%M:%S", modTime_hr)
+  return realTime
 
 
 def main():
