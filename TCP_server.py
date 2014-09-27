04c50cc254dfa2b7a9ed4206ff6f8051fa3c6cb6
Angel Hernandez <ah.sec7@gmail.com>
2014-09-27 11:05:54 -0500

    Added: Option to pass TIMEOUT argument from command line. More comments Result: Using signal.alarm() the server now gets terminated

--- TCP_server.py
+++ TCP_server.py
@@ -20,29 +20,34 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     print self.data
     # just send back the same data, but upper-cased
     self.request.sendall(self.data.upper())
-    signal.alarm(5)
+    # Second call of the signal.alarm method. Re-sets the timeout counter to the original timeout value
+    # Terminates the server if no connection is received in TIMEOUT seconds
+    signal.alarm(timeout)
 
+def create_server(HOST, PORT, timeout):
+  # Create the server, binding to localhost on port PORT
+  server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+  # First call to the signal.alarm method, Terminates the server if no connection is received in TIMEOUT seconds
+  signal.alarm(timeout)
+  # Activate the server; this will keep running until you interrupt the program with Ctrl-C
+  server.serve_forever()
 
 def main():
   args = sys.argv[1:]
-  if not args:
+  if len(args) < 2:
     print """
-Usage: FTP_server.py PORT
-This program sets a server that listens on port PORT
+Usage: FTP_server.py PORT TIMEOUT
+This program sets a server that listens on port PORT, if no connection is received in TIMEOUT seconds the server will be terminated
 """
 
   else:
     HOST = 'localhost'
     PORT = int(args[0])
+    global timeout
+    timeout = int(args[1])
+    create_server(HOST, PORT, timeout)
 
-    # Create the server, binding to localhost on port PORT
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-    # Set a 5-second alarm
-    signal.alarm(5)
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
-    
+# Standard Boiler Plate    
 if __name__ == "__main__":
   main()
 
