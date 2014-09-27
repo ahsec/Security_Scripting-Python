4a157b22e207e173b506386a099a50a3769f5b0b
Angel Hernandez <ah.sec7@gmail.com>
2014-09-27 10:12:07 -0500

    First partial of Exercise #1 Lesson # 6  SecurityTube Python Scripting Expert TCP server that listens for connections on an specific port passed as an argument from the console. Little variations from the one found at: https://docs.python.org/2/library/socketserver.html

---
+++ TCP_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import sys
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+
+  """
+  The RequestHandler class for our server.
+
+  It is instantiated once per connection to the server, and must
+  override the handle() method to implement communication to the
+  client.
+  """
+
+  def handle(self):
+    # self.request is the TCP socket connected to the client
+    self.data = self.request.recv(1024).strip()
+    print "{} wrote:".format(self.client_address[0])
+    print self.data
+    # just send back the same data, but upper-cased
+    self.request.sendall(self.data.upper())
+
+def main():
+  args = sys.argv[1:]
+  if not args:
+    print """
+Usage: FTP_server.py PORT
+This program sets a server that listens on port PORT
+"""
+
+  else:
+    HOST = 'localhost'
+    PORT = int(args[0])
+
+    # Create the server, binding to localhost on port PORT
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+  main()
+
+
