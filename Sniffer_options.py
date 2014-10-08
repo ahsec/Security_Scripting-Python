f7a52ade4052973acccc33296fb29eaf64904e09
Angel Hernandez <ah.sec7@gmail.com>
2014-10-07 20:23:38 -0500

    Sniffer Options is the program that calls the Sniffer in the Raw_Socket_Full It doesn end unexpectedly... that I have noticed so far ...

--- Sniffer_options.py
+++ Sniffer_options.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
 from StringIO import StringIO      # To redirect the Standard output to a variable
 import Raw_Socket_Full
 import sys
 
 def sniff_filtered(filter_tuple):
+  # From the arguments received we are trying to figure out what exactly the user wants to filter
+  # Options are either source or destination ports with a port number
   source_dest = filter_tuple[0]
   port = filter_tuple[1]
   str_to_filter = ''
@@ -12,20 +15,26 @@ def sniff_filtered(filter_tuple):
     str_to_filter = '-[Destination Port (2 bytes)]: '
   str_to_filter = str_to_filter + str(port)
   
-
-  # Store the reference, in case you want to show things again in standard output
-  old_stdout = sys.stdout
-  # This variable (result) will store everything that is sent to the standard output
-  result = StringIO()
-  sys.stdout = result
-  # Starting the sniffer (output will be redirected)
-  Raw_Socket_Full.start_sniffer()
-  # Redirect again the std output to screen
-  sys.stdout = old_stdout
-  result_value = result.getvalue()
-#  print '---- Full output: %s ' %(result_value)
-  if result_value.find(str_to_filter) > 0:
-    print result_value
+  # We are expecting to receive some errors ... -_-
+  try:
+    # Store the reference, in case you want to show things again in standard output
+    old_stdout = sys.stdout
+    # This variable (result) will store everything that is sent to the standard output
+    result = StringIO()
+    sys.stdout = result
+    # Doing some fancy stuff -- Starting the sniffer (output will be redirected)
+    Raw_Socket_Full.start_sniffer()
+    # Redirect again the std output to screen
+    sys.stdout = old_stdout
+    result_value = result.getvalue()
+  #  print '---- Full output: %s ' %(result_value)
+    if result_value.find(str_to_filter) > 0:
+      print result_value
+  except Exception as error:
+    print(error)
+    print 'Terinating Sniffer'
+    return -1
+    exit
   
 def main():
   args = sys.argv[1:]
@@ -41,7 +50,8 @@ def main():
       print 'Usage: Sniffer_options.py [s Source Port number to filter by |d Destination Port number to filter by]'
       return 0
       exit
-
+  # We are running the Sniffer FOREVER !!!
+  # Or until we receive a Ctrl + C ...
   while True:
     print sniff_filtered(filter_tuple)
     
