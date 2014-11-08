f9fcf6f6942c3dc174c47c7c32117b6f4ef3e285
Angel Hernandez <ah.sec7@gmail.com>
2014-11-07 21:41:05 -0600

    Second version: Instead of using a number for representing the maximum depth the crawler should go I'm using a number to represent the maximumm number of links the crawler should visit.
    That changed the logic of the program and made it easier. Recursiveness is working fine
    
    ToDo: Connect to a MySQL DB and write results to it

--- Web_Spider.py
+++ Web_Spider.py
@@ -4,100 +4,71 @@ import sys
 import urllib
 from bs4 import BeautifulSoup
 
+##########################################
+# SPSE Module 4. Lesson 7. Ex 1
+# A Web Crawler that writes the output to a DB
+# This code writes the output to a List of objects calle list_final_data
+# This program will download all the HTML files it finds in a specific URL. It will follow "num" number of links inside the original URL
+##########################################
+
+list_final_data = []
+url_to_visit = []
+
 def get_code_links_html(url):
+  # This function will return the html code and all the links (href) found in it
+  # This will be used later to visit the links found and retrieve more html code
   linksl = []
   reading = urllib.urlopen(url)
   code = reading.code
   if code != 200:
     html = 'Error. HTML cannot be read. %i' %(code)
   else:
-    html = reading.read()
     bs = BeautifulSoup(reading, 'lxml')
+    html = bs
     links = bs.find_all('a')
     for link in links:
-      if str(link).find('href') > 0:
+      if str(link).find('http') > 0:
         linksl.append(link['href'])
   return (code, html, linksl)
 
-#def visit_get_html(reading):
-#  #Function that visits a URL and returns it's html as string
-#  code = reading.code 
-#  if code != 200:
-#    html = 'Error. HTML cannot be read. %i' %(code)
-#  else: 
-#    html = reading.read()
-#  return html
-
-def verify_depth_go(url, depth):
-  decrease_depth()
-  code, html, links = get_code_links_html(url)
-  return (html, links)
-
-#def get_links(reading, depth):
-#  links = []
-#  bs = BeautifulSoup(reading, 'lxml')
-#  links = bs.find_all('a')
-#  for link in links:
-#    if str(link).find('href') > 0:
-#      links.append((depth, link['href']))
-#  return links
-
-def decrease_depth():
-  # Function to decrease the global variable "depth" by one
-  global depth 
-  # Equivalent to "depth = depth -1"
-  depth -= 1
-  print """********************************************
-Value of Depth : %i
-**********************""" %(depth)
-
 def usage():
   print """./Web_Spider.py URL num
-This program will download all the HTML files it finds in a specific URL. It follow "num" number levels inside the original URL"""
+This program will download all the HTML files it finds in a specific URL. It will follow "num" number of links inside the original URL"""
 
-def web_spider(url, depth):
-  # First section. Get the first url and html without verifying depth value
-#  code, html, links  = get_code_links_html(url)
-#  print """##############################################
-#URL, Depth, HTML
-###############################################
-#%s
-#%s
-#%s""" %(url, depth, html)
-  # Now, verify the value of depth and get the links from the html if necessary 
-  while (depth >= 0):
-    code, html, links  = get_code_links_html(url)
-    print """##############################################
-URL, Depth, HTML
-##############################################
-%s
-%s
-%s""" %(url, depth, html)
-    depth -= 1
-    for link in links:
-      if str(link).find('http') > 0:
-        print """++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
-%s 
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++""" %(link)
-        web_spider(link, depth)
-#    html, links = verify_depth_go(link, depth)
-#    print """##############################################
-#URL, Depth, HTML en ciclo
-##############################################
-#%s
-#%s
-#%s""" %(url, depth, html)
-  
+def web_spider(url, num_links):
+  # Main part of the program
+  global list_final_data 
+  global url_to_visit 
 
+  # While we haven't visited enough number of links (specified by command line) the program will call itself
+  while len(list_final_data) <= num_links:
+    # Get HTML code and links from URL
+    (code, html, linksl) = get_code_links_html(url)
+    # Add results to the "list_final_data" list. Which is the main results list
+    list_final_data.append((url, html))
+	###### Temporary - Verification pourpuses only
+    print '*' * 30 + 'Len List_Final_Data %i : \n ' %(len(list_final_data))
+    for element in list_final_data :
+      print element
+      print '*' * 30
+	###### Temporary - Verification pourpuses only
+    for element in linksl :
+      # From the elements in the links list retrieved from the HTML code 
+      # Will add each element to the beginning of the url_to_visit and then will visit 
+      # each site from that list (until reach the maximum number of links to visit)
+      url_to_visit.insert(0,element)
+      web_spider(url_to_visit.pop(), num_links)
+    
 def main():
   if len(sys.argv) < 3:
     usage()
   else:
+    # Get URL and number of links to follow
     args = sys.argv[1:]
     url = args[0]
-    global depth
-    depth = int(args[1])
-    web_spider(url, depth)
+    global num_links
+    num_links = int(args[1])
+    web_spider(url, num_links)
 
 if __name__ == '__main__':
   main()
